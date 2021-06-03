198ea7adde26a66b8a6c73efc80e3eb86dfdb12c
adrianhihihi <adrianhihi@gmail.com>
2021-06-04 00:36:20 +0800

    feat: implement asynchronous processing
    Using threads and a mutex, we can process Open Graph URLs asynchronously.
    
    Now we can do batch processing faster.
    
    However, there is no guarantee that the webpage can immediately give the
    processing result of the latest submission.
    
    For the latest results, we need to refresh the page manually after the system
    finishes processing.

--- app/controllers/og_previewer_controller.rb
+++ app/controllers/og_previewer_controller.rb
@@ -2,41 +2,57 @@ require 'faraday'
 require 'ogp'
 
 class OgPreviewerController < ApplicationController
-  @@results = Array.new
   ProcessResult = Struct.new(:complete_dt, :time_consuming, :status, :image_url)
+  @@results = Array.new
+  @@results_mutex = Mutex.new
 
   def index
     if params.key?(:clear)
-      @@results.clear
+      # Lock the mutex corresponding to results
+      # to prevent other threads from accessing it
+      @@results_mutex.synchronize do
+        # Clear history
+        @@results.clear
+      end
     end
 
     if params.key?(:submit_url) and params[:submit_url] != ""
-      watcher_start = Time.now
-      # og_url = 'https://ogp.me'
-      og_url = params[:submit_url]
+      # Asynchronous processing in a new thread
+      Thread.new do
+        watcher_start = Time.now
+        # og_url = 'https://ogp.me'
+        og_url = params[:submit_url]
 
-      begin
-        response = Faraday.get(og_url)
-        open_graph = OGP::OpenGraph.new(response.body)
-        status = "Success"
-      rescue Exception => e
-        status = "Failed: " + e.message
+        begin
+          response = Faraday.get(og_url)
+          open_graph = OGP::OpenGraph.new(response.body)
+          status = "Success"
+        rescue Exception => e
+          status = "Failed: " + e.message
+        end
+        watcher_stop = Time.now
+
+        # Calculate processing time
+        watcher_diff = watcher_stop - watcher_start
+
+        @@results_mutex.synchronize do
+          # Add the processing results to the @@results queue
+          @@results << ProcessResult.new(
+            DateTime.now.to_s,
+            watcher_diff.to_s + " s",
+            status,
+            open_graph != nil ? open_graph.image.url : ""
+          )
+        end
       end
-
-      watcher_stop = Time.now
-      watcher_diff = watcher_stop - watcher_start
-
-      @@results << ProcessResult.new(
-        DateTime.now.to_s,
-        watcher_diff.to_s + " s",
-        status,
-        open_graph != nil ? open_graph.image.url : ""
-      )
 
       redirect_to action: "index"
     end
 
-    @view_results = @@results.clone
+    @@results_mutex.synchronize do
+      # Clone the array to avoid potential data race
+      @view_results = @@results.clone
+    end
   end
 
 end
